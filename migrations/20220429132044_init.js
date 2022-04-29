74f9396e446cd5612f83427528e811ca214d8a4f
NikitaRagen <evgpool@yandex.ru>
2022-04-29 18:50:48 +0500

    db: add init game migration

---
+++ migrations/20220429132044_init.js
@@ -0,0 +1,123 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = (knex) => {
+  knex.schema.createTable('users', (table) => {
+    table
+      .increments('id')
+      .primary()
+      .comment('Идентификатор');
+    table
+      .string('login', 256)
+      .notNullable()
+      .unique()
+      .comment('Логин');
+    table
+      .string('password', 2048)
+      .notNullable()
+      .comment('Пароль');
+    table
+      .string('status', 64)
+      .notNullable()
+      .defaultTo('active')
+      .comment('Статус');
+    table
+      .timestamp('created_at', {useTz: false})
+      .notNullable()
+      .defaultTo(knex.fn.now())
+      .comment('Дата создания');
+    table
+      .timestamp('updated_at', {useTz: false})
+      .nullable()
+      .comment('Дата обновления');
+    table.comment('Пользователи');
+  });
+  knex.schema.createTable('games', (table) => {
+    table
+      .increments('id')
+      .primary()
+      .comment('Идентификатор');
+    table
+      .integer('size')
+      .notNullable()
+      .comment('Размер поля');
+    table
+      .integer('winner_number')
+      .nullable()
+      .comment('Номер победившего игрока');
+    table
+      .timestamp('created_at', {useTz: false})
+      .notNullable()
+      .defaultTo(knex.fn.now())
+      .comment('Дата создания');
+    table
+      .timestamp('finished_at', {useTz: false})
+      .nullable()
+      .comment('Дата окончания');
+    table
+      .timestamp('deleted_at', {useTz: false})
+      .nullable()
+      .comment('Дата удаления');
+    table.comment('Игры');
+  });
+  knex.schema.createTable('players', (table) => {
+    table
+      .increments('id')
+      .primary()
+      .comment('Идентификатор');
+    table
+      .integer('user_id')
+      .notNullable()
+      .references('id')
+      .inTable('users')
+      .comment('Идентификатор пользователя');
+    table
+      .integer('game_id')
+      .notNullable()
+      .references('id')
+      .inTable('games')
+      .comment('Идентификатор игры');
+    table
+      .integer('number')
+      .notNullable()
+      .comment('Номер игрока по порядку');
+    table
+      .unique(['user_id', 'game_id'])
+      .unique(['game_id', 'number'])
+      .comment('Игроки');
+  });
+  knex.schema.createTable('moves', (table) => {
+    table
+      .increments('id')
+      .primary()
+      .comment('Идентификатор');
+    table
+      .integer('game_id')
+      .notNullable()
+      .comment('Идентификатор игры');
+    table
+      .integer('number')
+      .notNullable()
+      .comment('Номер хода по порядку');
+    table
+      .integer('position')
+      .notNullable()
+      .comment('Позиция на которую сделан ход');
+    table
+      .unique(['game_id', 'number'])
+      .unique(['game_id', 'position'])
+      .comment('Ходы');
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = (knex) => {
+  knex.schema.dropTable('moves');
+  knex.schema.dropTable('players');
+  knex.schema.dropTable('games');
+  knex.schema.dropTable('users');
+};
