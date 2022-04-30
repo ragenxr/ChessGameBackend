9d6067f3b95f447875ad731eae8594fa2d14e598
NikitaRagen <evgpool@yandex.ru>
2022-04-30 22:22:55 +0500

    feat: extend query

--- src/utils/query.js
+++ src/utils/query.js
@@ -5,10 +5,29 @@ const operatorMap = {
   lt: '<',
   gte: '>=',
   lte: '<=',
-  in: 'in',
-  notin: 'not in',
-  is: 'is',
-  isnot: 'is not'
+  pr: 'in',
+  npr: 'not in',
+  co: 'like',
+  sw: 'like',
+  ew: 'like'
+};
+
+const parseValue = (operator, value) => {
+  if (['pr', 'npr'].includes(operator)) {
+    return value.split(',');
+  } else if (operator === 'co') {
+    return `%${value}%`;
+  } else if (operator === 'sw') {
+    return `${value}%`;
+  } else if (operator === 'ew') {
+    return `%${value}`;
+  } else {
+    try {
+      return JSON.parse(value);
+    } catch (err) {
+      return value;
+    }
+  }
 };
 
 /**
@@ -17,17 +36,17 @@ const operatorMap = {
  * @return {{operator: string, left: string, value: *}[]}
  */
 const parseFilter = (filterQuery) => filterQuery
-    .split(';')
+    .split('and')
     .reduce(
       (filters, rawFilter) => {
-        const [left, operator, right] = rawFilter.trim().split(' ');
+        const [left, operator, right] = rawFilter.trim().split(' ', 3);
 
         return [
           ...filters,
           {
-            operator: operatorMap[operator] || operator,
+            operator: operatorMap[operator],
             left,
-            right: right === 'null' ? null : right
+            right: parseValue(operator, right)
           }
         ];
       },
