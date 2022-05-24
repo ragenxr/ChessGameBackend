33b2a8c9fb226228264e8142d82e237a4613c189
NikitaRagen <evgpool@yandex.ru>
2022-05-24 21:52:32 +0500

    feat: finish

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,7 @@
 const config = require('./configs');
 
 module.exports = {
-  development: config.development.database
+  development: config.development.database,
+  stage: config.stage.database,
+  production: config.production.database
 };
